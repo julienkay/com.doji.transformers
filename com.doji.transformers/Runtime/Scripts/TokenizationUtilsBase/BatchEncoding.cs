68b831958bdb6270ca13282e4acb37065d8f60e3
Julien Kipp <julien@doji-tech.com>
2024-01-07 23:54:42 +0100

    convenience accessor

--- com.doji.transformers/Runtime/Scripts/TokenizationUtilsBase/BatchEncoding.cs
+++ com.doji.transformers/Runtime/Scripts/TokenizationUtilsBase/BatchEncoding.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace Doji.AI.Transformers {
 
     /// <summary>
-    /// Holds the output of <see cref="PreTrainedTokenizerBase.EncodePrompt"/>
+    /// Holds the output of <see cref="PreTrainedTokenizerBase.Encode"/>
     /// and <see cref="PreTrainedTokenizerBase.EncodePlus"/> methods
     /// (tokens, attention_masks, etc).
     /// 
@@ -12,21 +12,23 @@ namespace Doji.AI.Transformers {
     /// </summary>
     public class BatchEncoding : Dictionary<string, object> {
 
+        public List<int> InputIds {
+            get {
+                TryGetValue("input_ids", out object inputIds);
+                return inputIds as List<int>;
+            }
+        }
         public bool PrependBatchAxis { get; set;}
         public int? NSequences { get; set; }
 
         public BatchEncoding(
             Dictionary<string, object> data = null,
-            object encoding = null, // dummy, EmcodingFast implemented in the `tokenizers` library.
+            object encoding = null, // dummy, EmcodingFast implemented in the `tokenizers` library, which is not yet used
             bool prependBatchAxis = false,
             int? nSequences = null) : base(data)
         {
             PrependBatchAxis = prependBatchAxis;
             NSequences = nSequences;
         }
-
-        /*public static implicit operator BatchEncoding(Dictionary<string, object> data) {
-            return new BatchEncoding(data);
-        }*/
     }
 }
