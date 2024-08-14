a25824385095d271a0a249852bb22747b27f5a4f
Julien Kipp <julien@doji-tech.com>
2024-08-14 20:42:35 +0200

    proper test script

--- com.doji.transformers/Runtime/Scripts/Models/Phi3/Phi3ForCausalLM.cs
+++ com.doji.transformers/Runtime/Scripts/Models/Phi3/Phi3ForCausalLM.cs
@@ -10,8 +10,6 @@ namespace Doji.AI.Transformers {
         protected override bool AcceptsAttentionMask => true;
         protected override bool HasEncoder => false;
 
-        private Dictionary<string, Tensor> _inputs = new Dictionary<string, Tensor>();
-
         public Phi3ForCausalLM(Model model, PretrainedConfig config, BackendType backend = BackendType.GPUCompute) : base(model, config, backend) { }
 
         /// <summary>
@@ -26,40 +24,6 @@ namespace Doji.AI.Transformers {
             _worker.Execute(tensorInputs);
             var logits = _worker.PeekOutput("logits") as TensorFloat;
             return new CausalLMOutputWithPast(logits);
-        }
-
-        public TensorFloat Execute(TensorInt inputIds, TensorInt attentionMask, TensorInt positionIds) {
-            _inputs["input_ids"] = inputIds;
-            _inputs["attention_mask"] = attentionMask;
-            _inputs["position_ids"] = positionIds;
-            _worker.Execute(_inputs);
-            return _worker.PeekOutput("logits") as TensorFloat;
-        }
-
-        public TensorFloat ExecuteLayerByLayer(TensorInt inputIds, TensorInt attentionMask, TensorInt positionIds) {
-            _inputs["input_ids"] = inputIds;
-            _inputs["attention_mask"] = attentionMask;
-            _inputs["position_ids"] = positionIds;
-
-            var schedule = _worker.ExecuteLayerByLayer(_inputs);
-            int i = 0;
-            bool loop = true;
-            while (loop) {
-                if (i == 32) {
-                    ;
-                }
-                try {
-                    loop = schedule.MoveNext();
-                } catch (Exception e) {
-                    UnityEngine.Debug.LogError(e);
-                    UnityEngine.Debug.LogError(i);
-
-                    break;
-                }
-                i++;
-            }
-
-            return _worker.PeekOutput("logits") as TensorFloat;
         }
 
         protected override Dictionary<string, object> PrepareInputsForGeneration(
--- projects/Transformers/Assets/TestPhi3.cs
+++ projects/Transformers/Assets/TestPhi3.cs
@@ -1,4 +1,5 @@
 using Doji.AI.Transformers;
+using System.IO;
 using System.Linq;
 using Unity.Sentis;
 using UnityEngine;
@@ -6,48 +7,31 @@ using UnityEngine;
 public class TestPhi3 : MonoBehaviour {
 
     private IBackend _backend;
-    PreTrainedTokenizerBase _tokenizer;
-    Phi3ForCausalLM _model;
+    private PreTrainedTokenizerBase _tokenizer;
+    private Phi3ForCausalLM _model;
 
     private void Start() {
         string modelId = "julienkay/Phi-3-mini-4k-instruct_no_cache_uint8";
-        var prompt = "This is an example script .";
+        var prompt = "This is a sample script .";
 
         _backend = WorkerFactory.CreateBackend(BackendType.GPUCompute);
         _tokenizer = AutoTokenizer.FromPretrained(modelId);
         _model = Phi3ForCausalLM.FromPretrained(modelId);
 
-        var tokens = _tokenizer.Encode(prompt);
-        Debug.Log(string.Join(", ", tokens.InputIds));
-
-        int SEQUENCE_LENGTH = 4096;//?
-        var sample = prompt;
-        for (int i = 0; i < 10; i++) {
-            var encodings = _tokenizer.Encode(sample);
-            var input_ids = encodings.InputIds.ToArray();
-            var attention_mask = encodings.AttentionMask.ToArray();
-            //var token_type_ids = encodings.TokenTypeIds.ToArray();
+        var encodings = _tokenizer.Encode(prompt);
+        var inputIds = encodings.InputIds.ToArray();
+        //var attention_mask = encodings.AttentionMask.ToArray();
+        //var token_type_ids = encodings.TokenTypeIds.ToArray();
             
-            if (input_ids.Length >= SEQUENCE_LENGTH - 1) break;
-            
-            using TensorInt input_tensor = new TensorInt(new TensorShape(1, input_ids.Length), input_ids);
-            using TensorInt attention_mask_tensor = new TensorInt(new TensorShape(1, attention_mask.Length), attention_mask);
-            using TensorInt position_ids_tensor = new TensorInt(new TensorShape(1, input_ids.Length), Enumerable.Range(0, input_ids.Length).ToArray());//??
-            var predictions = _model.Execute(input_tensor, attention_mask_tensor, position_ids_tensor);
-            int[] nextToken = SampleNext(predictions);
-            sample += ' ' + _tokenizer.Decode(nextToken.ToList());
-        }
-        Debug.Log(sample);
-    }
-
-    private int[] SampleNext(TensorFloat predictions) {
-        // Greedy approach.
-        using TensorFloat probabilities = TensorFloat.AllocNoData(predictions.shape);
-        _backend.Softmax(predictions, probabilities, axis: -1);
-        using TensorInt next_token = TensorInt.AllocNoData(probabilities.shape.Reduce(-1, false));
-        _backend.ArgMax(probabilities, next_token, -1, false);
-        using TensorInt pred = next_token.ReadbackAndClone();
-        return pred.ToReadOnlyArray();
+        using TensorInt inputTensor = new TensorInt(new TensorShape(1, inputIds.Length), inputIds);
+        var generationConfig = GenerationConfig.Deserialize(File.ReadAllText("Assets/StreamingAssets/julienkay/Phi-3-mini-4k-instruct_no_cache_uint8/generation_config.json"));
+        generationConfig.MaxNewTokens = 30;
+        var result = _model.Generate(inputTensor, generationConfig);
+        var seq = result.Get<TensorInt>("sequences");
+        seq = seq.ReadbackAndClone();
+        string output = _tokenizer.Decode(seq.ToReadOnlyArray().ToList(), skipSpecialTokens: true, cleanUpTokenizationSpaces: false);
+        Debug.Log("Predicted IDs: " + string.Join(", ", seq.ToReadOnlyArray()));
+        Debug.Log(output);
     }
 
     private void OnDestroy() {
