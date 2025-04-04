2492ae5f44629f5353b6d83bda1be68bd9a1574b
Julien Kipp <julien@doji-tech.com>
2025-04-04 20:09:38 +0200

    bump version

--- com.doji.transformers/Runtime/AssemblyInfo.cs
+++ com.doji.transformers/Runtime/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("Doji.Transformers")]
 [assembly: AssemblyCopyright("Copyright © Julien Kipp 2023-2024")]
 
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
-[assembly: AssemblyInformationalVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyInformationalVersion("0.1.3")]
 [assembly: InternalsVisibleTo("Doji.Transformers.Editor.Tests")]
