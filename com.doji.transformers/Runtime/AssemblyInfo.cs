47e4ea1e55a7c3cfe397389c2de66e149d420c75
Julien Kipp <julien@doji-tech.com>
2025-04-10 21:37:38 +0200

    bump version

--- com.doji.transformers/Runtime/AssemblyInfo.cs
+++ com.doji.transformers/Runtime/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Run pretrained transformer models with Unity Sentis")]
 [assembly: AssemblyCompany("Doji Technologies")]
 [assembly: AssemblyProduct("Doji.Transformers")]
-[assembly: AssemblyCopyright("Copyright © Julien Kipp 2023-2024")]
+[assembly: AssemblyCopyright("Copyright © Julien Kipp 2023-2025")]
 
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
-[assembly: AssemblyInformationalVersion("0.1.4")]
+[assembly: AssemblyVersion("0.1.5.0")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
+[assembly: AssemblyInformationalVersion("0.1.5")]
 [assembly: InternalsVisibleTo("Doji.Transformers.Editor.Tests")]
